8adf1b74cc737c9f6040f27f5c3e5c16412eede5
Psycho <mohamadrobatjazi1387@gmail.com>
2022-09-01 00:43:47 +0430

    Construction of project tables _ fix bug

---
+++ Seo_Automation/database/migrations/2022_08_31_172450_create_urls_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('urls', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('path');
+            $table->string('title')->nullable();
+            $table->string('meta_description')->nullable();
+            $table->string('canonical')->nullable();
+            $table->string('http_status_code');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('urls');
+    }
+};
